17a73faad6e907b9514c3ae259919956e9219f3c
Dennis Oelkers <dennis@lauschmusik.de>
2011-08-19 13:53:58 +0200

    - added processing queue class

--- start.js
+++ start.js
@@ -2,8 +2,14 @@ var config = require('./config').config();
 var debug_port = require('./lib/debug_port');
 var datastorage = require('./lib/datastorage');
 var gelf_listener = require('./lib/gelf_listener');
+var ProcessingQueue = require('./lib/processing_queue');
 var server = {};
 
-server['datastorage'] = datastorage.init(config['db']);
-server['gelf_listener'] = gelf_listener.start(config['gelf_port'], datastorage);
-server['debug_port'] = debug_port.start(config['debug_port'], server);
+datastorage.init(config['db']);
+processingQueue = new ProcessingQueue(datastorage);
+gelf_listener.start(config['gelf_port'], processingQueue);
+debug_port.start(config['debug_port'], server);
+
+server['datastorage'] = datastorage;
+server['gelf_listener'] = gelf_listener;
+server['debug_port'] = debug_port;
