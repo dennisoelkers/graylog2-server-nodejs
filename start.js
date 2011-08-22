eddf70e6b7dd6ef6d31c0e9fe15b9ba8b7969af5
Dennis Oelkers <dennis@lauschmusik.de>
2011-08-22 21:42:07 +0200

    - added processing queue - changed data storage module to prototype

--- start.js
+++ start.js
@@ -1,15 +1,19 @@
 var config = require('./config').config();
 var debug_port = require('./lib/debug_port');
-var datastorage = require('./lib/datastorage');
+var DataStorage = require('./lib/datastorage');
 var gelf_listener = require('./lib/gelf_listener');
 var ProcessingQueue = require('./lib/processing_queue');
+var Worker = require('./lib/worker');
 var server = {};
 
-datastorage.init(config['db']);
-processingQueue = new ProcessingQueue(datastorage);
+dataStorage = new DataStorage(config['db']);
+processingQueue = new ProcessingQueue();
 gelf_listener.start(config['gelf_port'], processingQueue);
+worker = new Worker(processingQueue, dataStorage);
+
 debug_port.start(config['debug_port'], server);
 
-server['datastorage'] = datastorage;
+server['datastorage'] = dataStorage;
 server['gelf_listener'] = gelf_listener;
 server['debug_port'] = debug_port;
+server['worker'] = worker;
