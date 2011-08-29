0ce07931254531ccad9ad78e79e5cc5e18a86e4c
Dennis Oelkers <dennis@lauschmusik.de>
2011-08-30 01:18:51 +0200

    - added basics of stream matching - fixed missing fields in test sender

--- testsender.js
+++ testsender.js
@@ -6,22 +6,26 @@ var message = {
 	host : 'host.domain.tld',
 	short_message : 'this is a short test message',
 	full_message : 'this is a long test message',
-	facility : 'testsender.js'
+	facility : 'testsender.js',
+	file : 'testsender.js',
+	line : 42,
+	level : 3
 }
 
 var stats = {
 	count: 0
 }
 
-var buf = GELF.encode(message)
 setInterval(function(stats) {
 	console.log("Count: " + stats['count']);
-}, stats);
+}, 2000, stats);
 
-//setInterval(function(client, buf, stats) {
+setInterval(function(client, stats) {
 while(true) {
+	message['created_at'] = new Date().getTime() / 1000;
+	var buf = GELF.encode(message)
 	client.send(buf, 0, buf.length, 12201, 'localhost');
 	stats['count'] += 1;
 	console.log(stats['count']);
 }
-//}, 1, client, buf, stats);
+}, 100, client, stats);
