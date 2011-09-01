893aeb67f69f82e6f72e1f3ff1da7464e9d2d882
Dennis Oelkers <dennis@lauschmusik.de>
2011-09-01 16:46:09 +0200

    - basic proof of concept for submission to elastic search indexing server, you need to do an npm install elasticsearchclient

--- config.js
+++ config.js
@@ -7,7 +7,14 @@ function config() {
 			"collection" : "messages"
 		},
 		"debug_port" : 2342,
-		"gelf_port" : 12201
+		"gelf_port" : 12201,
+		"indexer" : {
+			"host" : "localhost",
+			"port" : 9200,
+			"type" : "elasticsearch",
+			"index" : "graylog2test",
+			"type" : "messages"
+		}
 	}
 }
 
